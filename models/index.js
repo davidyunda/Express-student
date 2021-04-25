b21f87db6ce5a9234fe227c803a5bdf37824d7b6
Your Name <you@example.com>
2021-04-25 18:30:26 -0500

    student sign in with DB

---
+++ models/index.js
@@ -0,0 +1,33 @@
+let { Sequelize, DataTypes } = require('sequelize')
+
+let env = process.env.NODE_ENV || 'development'
+
+// if app is running at Heroku, Heroku will have set an environment
+// variable called NODE_ENV which will have the value 'production'
+// so the env variable in this production will be 'production'
+
+// if the app is running in your computer, then env wil be 'development
+// app will use SQLite
+
+let config = require(__dirname + '/../config.json')[env]
+
+let db = {}
+
+let sequelize
+
+if (config.use_env_variable) {
+    // at Heroku, use postgres
+    sequelize = new Sequelize(process.env[config.use_env_variable], config)
+} else {
+    // running locally, development mode, use SQLite
+    sequelize = new Sequelize(config)
+}
+
+let studentModel = require('./student')(sequelize, DataTypes)
+
+db[studentModel.name] = studentModel
+
+db.sequelize = sequelize // information on how to connect to the dababase
+db.Sequelize = Sequelize // reference to sequelize library
+
+module.exports = db
